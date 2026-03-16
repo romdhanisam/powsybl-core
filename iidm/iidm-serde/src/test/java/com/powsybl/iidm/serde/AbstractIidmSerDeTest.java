10c95b7004772b5fc8f6402aff462e9f9c3f9a90
Samir Romdhani <samir.romdhani_externe@rte-france.com>
2026-03-16 21:39:24 +0100

    fix TiLine with Anonymizer
    Signed-off-by: Samir Romdhani <romdhanisamir94@gmail.com>

--- iidm/iidm-serde/src/test/java/com/powsybl/iidm/serde/AbstractIidmSerDeTest.java
+++ iidm/iidm-serde/src/test/java/com/powsybl/iidm/serde/AbstractIidmSerDeTest.java
@@ -179,6 +179,13 @@ public abstract class AbstractIidmSerDeTest extends AbstractSerDeTest {
                 .forEach(test);
     }
 
+    protected void testForAllVersionsBetween(IidmVersion minVersion, IidmVersion maxVersion, Consumer<IidmVersion> test) {
+        Stream.of(IidmVersion.values())
+                .filter(v -> v.compareTo(minVersion) >= 0
+                        && v.compareTo(maxVersion) <= 0)
+                .forEach(test);
+    }
+
     /**
      * Execute a write test for the given network, for all IIDM versions strictly older than a given maximum IIDM
      * version, and compare to the given versioned xml reference test resource.
--- iidm/iidm-serde/src/test/java/com/powsybl/iidm/serde/TieLineTerminalRefBugTest.java
+++ iidm/iidm-serde/src/test/java/com/powsybl/iidm/serde/TieLineTerminalRefBugTest.java
@@ -7,12 +7,17 @@
  */
 package com.powsybl.iidm.serde;
 
+import com.powsybl.iidm.network.Network;
+import com.powsybl.iidm.network.test.EurostagTutorialExample1Factory;
+import com.powsybl.iidm.serde.anonymizer.Anonymizer;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 import static com.powsybl.iidm.serde.IidmSerDeConstants.CURRENT_IIDM_VERSION;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author Geoffroy Jamgotchian {@literal <geoffroy.jamgotchian at rte-france.com>}
@@ -22,5 +27,39 @@ class TieLineTerminalRefBugTest extends AbstractIidmSerDeTest {
     @Test
     void test() throws IOException {
         assertDoesNotThrow(() -> NetworkSerDe.read(getClass().getResourceAsStream(getVersionedNetworkPath("tieLineTerminalRefBug.xml", CURRENT_IIDM_VERSION))));
+    }
+
+    @Test
+    void importNetworkWithTiLineAndAnonymizeInNewerVersionShouldSucceed() {
+        Network network = EurostagTutorialExample1Factory.createWithTieLinesAndAreas();
+        testForAllVersionsSince(IidmVersion.V_1_16, version -> {
+            ExportOptions exportOptions = new ExportOptions().setVersion(version.toString(".")).setAnonymized(true);
+            // Export (with Anonymize option)
+            ByteArrayOutputStream os = new ByteArrayOutputStream();
+            Anonymizer anonymizer = NetworkSerDe.write(network, exportOptions, os);
+            // Import (with Anonymize)
+            //Before
+//            PowsyblException e = assertThrows(PowsyblException.class, () -> NetworkSerDe.read(new ByteArrayInputStream(os.toByteArray()), new ImportOptions(), anonymizer));
+//            assertEquals("AC tie Line 'NHV1_NHV2_1': J and/or Q are not boundary lines in the network", e.getMessage());
+            //After fix
+            assertDoesNotThrow(() -> NetworkSerDe.read(new ByteArrayInputStream(os.toByteArray()), new ImportOptions(), anonymizer));
+        });
+    }
+
+    @Test
+    void importNetworkWithTiLineAndAnonymizeInOlderVersionShouldSucceed() {
+        Network network = EurostagTutorialExample1Factory.createWithTieLinesAndAreas();
+        testForAllVersionsBetween(IidmVersion.V_1_10, IidmVersion.V_1_15, version -> {
+            ExportOptions exportOptions = new ExportOptions().setVersion(version.toString(".")).setAnonymized(true);
+            // Export (with Anonymize option)
+            ByteArrayOutputStream os = new ByteArrayOutputStream();
+            Anonymizer anonymizer = NetworkSerDe.write(network, exportOptions, os);
+            // Import (with Anonymize)
+            //Before
+//            PowsyblException e = assertThrows(PowsyblException.class, () -> NetworkSerDe.read(new ByteArrayInputStream(os.toByteArray()), new ImportOptions(), anonymizer));
+//            assertEquals("AC tie Line 'NHV1_NHV2_1': J and/or Q are not boundary lines in the network", e.getMessage());
+            // After fix
+            assertDoesNotThrow(() -> NetworkSerDe.read(new ByteArrayInputStream(os.toByteArray()), new ImportOptions(), anonymizer));
+        });
     }
 }
